40aa55f28d88837105bee2448210f1b5a837bce0
Master Joy <386442876@qq.com>
2024-03-18 13:55:31 +0800

    优化：svc 生成

--- generator/gen_svc.go
+++ generator/gen_svc.go
@@ -3,20 +3,13 @@ package generator
 import (
 	. "github.com/MasterJoyHunan/genrpc/prepare"
 	"github.com/MasterJoyHunan/genrpc/tpl"
-
-	"github.com/zeromicro/go-zero/tools/goctl/util/format"
 )
 
 func GenSvc() error {
-	filename, err := format.FileNamingFormat("go_zero", GrpcProto.Package.Name)
-	if err != nil {
-		return err
-	}
-
 	return genFile(fileGenConfig{
 		dir:             GrpcOutDir,
 		subDir:          "svc",
-		filename:        filename + ".go",
+		filename:        "grpc_context.go",
 		templateName:    "grpcContextTemplate",
 		builtinTemplate: tpl.GrpcContext,
 	})
--- test/example/svc/grpc_context.go
+++ test/example/svc/grpc_context.go
@@ -1,32 +1,32 @@
 package svc
 
 import (
-    "context"
-    "time"
+	"context"
+	"time"
 )
 
 type GrpcContext struct {
-    grpcCtx context.Context
+	grpcCtx context.Context
 }
 
 func NewGrpcContext(grpcCtx context.Context) *GrpcContext {
-    return &GrpcContext{
-        grpcCtx: grpcCtx,
-    }
+	return &GrpcContext{
+		grpcCtx: grpcCtx,
+	}
 }
 
 func (c *GrpcContext) Deadline() (deadline time.Time, ok bool) {
-    return c.grpcCtx.Deadline()
+	return c.grpcCtx.Deadline()
 }
 
 func (c *GrpcContext) Done() <-chan struct{} {
-    return c.grpcCtx.Done()
+	return c.grpcCtx.Done()
 }
 
 func (c *GrpcContext) Err() error {
-    return c.grpcCtx.Err()
+	return c.grpcCtx.Err()
 }
 
 func (c *GrpcContext) Value(key any) any {
-    return c.grpcCtx.Value(key)
+	return c.grpcCtx.Value(key)
 }
